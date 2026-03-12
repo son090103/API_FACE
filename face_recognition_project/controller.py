3b61e2c2d689c078583e23afb51a76f464ec43d1
son090103 <144188086+son090103@users.noreply.github.com>
2026-03-12 17:19:24 +0700

    first commit

--- face_recognition_project/controller.py
+++ face_recognition_project/controller.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, APIRouter
 from pydantic import BaseModel
-from face_login import face_login
-from face_register import face_register 
+from face_recognition_project.face_login import face_login
+from face_recognition_project.face_register import face_register
 # ================= APP =================
 app = FastAPI()
 
