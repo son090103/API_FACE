b6720f4e4661c38c3a17d3c7e2629f2689fa4943
son090103 <144188086+son090103@users.noreply.github.com>
2026-03-27 00:52:42 +0700

    update

--- face_recognition_project/face_model.py
+++ face_recognition_project/face_model.py
@@ -1,14 +1,15 @@
 from insightface.app import FaceAnalysis
 
-_face_app = None
+# 🔥 load ngay khi import
+face_app = FaceAnalysis(
+    name="buffalo_s",
+    providers=["CPUExecutionProvider"]
+)
+
+face_app.prepare(ctx_id=0, det_size=(640, 640))
+
+print("✅ InsightFace model loaded (CPU)")
+
 
 def get_face_app():
-    global _face_app
-    if _face_app is None:
-        _face_app = FaceAnalysis(
-            name="buffalo_s",
-            providers=["CPUExecutionProvider"]  # Railway không có GPU
-        )
-        _face_app.prepare(ctx_id=0, det_size=(640, 640))
-        print("✅ InsightFace model loaded (CPU)")
-    return _face_app
+    return face_app
