16ae06ae3d789bc269df3f68313e9bb67f74088b
son090103 <144188086+son090103@users.noreply.github.com>
2026-03-27 00:48:16 +0700

    update

--- face_recognition_project/face_model.py
+++ face_recognition_project/face_model.py
@@ -6,7 +6,7 @@ def get_face_app():
     global _face_app
     if _face_app is None:
         _face_app = FaceAnalysis(
-            name="buffalo_l",
+            name="buffalo_s",
             providers=["CPUExecutionProvider"]  # Railway không có GPU
         )
         _face_app.prepare(ctx_id=0, det_size=(640, 640))
